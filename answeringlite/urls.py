f66390cd074a37834ced8d873fc51ca01f4a7efb
LuciferTM <roy_qian@gmail.com>
2017-07-19 10:11:03 +0800

    封装一下

--- answeringlite/urls.py
+++ answeringlite/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
+
+def gikoo_url(regex, view, kwargs=None, name=None, prefix=''):
+    url_prefix = '^' + settings.GIKOO['api_url_prefix'][1:]
+    regex = url_prefix + regex
+    return url(regex, view, kwargs, name, prefix)
 
 urlpatterns = patterns('',
     # Examples:
@@ -14,4 +19,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    gikoo_url(r'wechat/', include('wechat.urls')),
 )
